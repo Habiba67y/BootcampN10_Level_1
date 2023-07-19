021b1e76c52a044cb8fecd9918d063281ee83cf9
Habiba67y <sattorovahabiba00@gmail.com>
2023-07-19 15:08:15 +0500

    #7-uyga vazifa tugatildi#

--- N7_HT1/Program.cs
+++ N7_HT1/Program.cs
@@ -50,3 +50,51 @@ for (int i = 0; i < 5; i++)
     sleepQuality[i] = (duration[i].TotalHours - awakingIndex[i]) / (8 + previousDayMissingSleep[i].TotalHours) * 10;
     Console.WriteLine($"{sleepDays[i]} - {duration[i]} - {Convert.ToSingle(sleepQuality[i])}");
 }
+Console.WriteLine();
+DateTime now = DateTime.Now;
+DateOnly s;
+for (int i = 0; i < 4; i++)
+{
+    for (int j = i + 1; j < 5; j++)
+    {
+        if (((now.Day - sleepDays[i].Day)<=0) &&((now.Day - sleepDays[j].Day) <= 0))
+        {
+            if ((now.Day - sleepDays[i].Day) > (now.Day - sleepDays[j].Day))
+            {
+                s = sleepDays[i];
+                sleepDays[i] = sleepDays[j];
+                sleepDays[j] = s;
+            }
+        }
+        if (((now.Day - sleepDays[i].Day) >= 0) && ((now.Day - sleepDays[j].Day) >= 0))
+        {
+            if ((now.Day - sleepDays[i].Day) < (now.Day - sleepDays[j].Day))
+            {
+                s = sleepDays[i];
+                sleepDays[i] = sleepDays[j];
+                sleepDays[j] = s;
+            }
+        }
+        if (((now.Day - sleepDays[i].Day) <= 0) && ((now.Day - sleepDays[j].Day) >= 0))
+        {
+            if ((sleepDays[i].Day - now.Day) < (now.Day - sleepDays[j].Day))
+            {
+                s = sleepDays[i];
+                sleepDays[i] = sleepDays[j];
+                sleepDays[j] = s;
+            }
+        }
+        if (((now.Day - sleepDays[i].Day) >= 0) && ((now.Day - sleepDays[j].Day) <= 0))
+        {
+            if ((now.Day - sleepDays[i].Day) < (sleepDays[j].Day - now.Day))
+            {
+                s = sleepDays[i];
+                sleepDays[i] = sleepDays[j];
+                sleepDays[j] = s;
+            }
+        }
+
+    }
+    Console.WriteLine(sleepDays[i]);
+}
+Console.WriteLine(sleepDays[4]);
