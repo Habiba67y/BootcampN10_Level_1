de7d487c8aa863e7c1873e15bfedc9875bba305d
Habiba67y <sattorovahabiba00@gmail.com>
2023-07-28 15:31:06 +0500

    #14-darsni tasklari bajarildi#

---
+++ N14_T3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+var v = new Validator();
+Console.Write("Name: ");
+Console.WriteLine(v.isValidname(Console.ReadLine()));
+Console.Write("Email address: ");
+Console.WriteLine(v.isValidEmailAddress(Console.ReadLine()));
+Console.Write("Phone number: ");
+Console.WriteLine(v.isValidPhoneNumber(Console.ReadLine()));
+public class Validator
+{
+    private Regex name = new Regex("^[a-zA-Z'-]+$");
+    private Regex email = new Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+    private Regex phone = new Regex("^\\+?\\d{1,3}?[-.\\s]?\\(?\\d{1,4}\\)?[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,9}$");
+    public bool isValidname(string n)
+    {
+        return name.IsMatch(n);
+    }
+    public bool isValidEmailAddress(string e)
+    {
+        return email.IsMatch(e);
+    }
+    public bool isValidPhoneNumber(string p)
+    {
+        return phone.IsMatch(p);
+    }
+}
---
+++ N14_T4/Program.cs
@@ -0,0 +1,66 @@
+﻿Console.WriteLine("To'g'ri to'rtburchak tomonlarni kiriting:");
+Console.Write("a = ");
+var a = int.Parse(Console.ReadLine());
+Console.Write("b = ");
+var b = int.Parse(Console.ReadLine());
+var r = new Rectangle(a, b);
+r.CalculateArea();
+Console.WriteLine("Uchburchak balandligi va asosini kiriting:");
+Console.Write("h = ");
+var h = int.Parse(Console.ReadLine());
+Console.Write("b = ");
+var b1 = int.Parse(Console.ReadLine());
+var t = new Triangle(h, b1);
+t.CalculateArea();
+Console.WriteLine("Doira radiusini kiriting:");
+Console.Write("r = ");
+var r1 = int.Parse(Console.ReadLine());
+var c = new Circle(r1);
+c.CalculateArea();
+public class Shape
+{
+    public virtual void CalculateArea()
+    {
+        Console.WriteLine("Calculating area");
+    }
+}
+public class Rectangle : Shape
+{
+    public float Tomon1;
+    public float Tomon2;
+    public Rectangle(float tomon1, float tomon2)
+    {
+        Tomon1 = tomon1;
+        Tomon2 = tomon2;
+    }
+    public override void CalculateArea()
+    {
+        Console.WriteLine($"Area of rectangle: {Tomon1 * Tomon2}");
+    }
+}
+public class Triangle : Shape
+{
+    public float balandlik;
+    public float asos;
+    public Triangle(float b, float h)
+    {
+        balandlik = h;
+        asos = b;
+    }
+    public override void CalculateArea()
+    {
+        Console.WriteLine($"Area of triangle: {balandlik*asos*0.5}");
+    }
+}
+public class Circle : Shape
+{
+    public float radius;
+    public Circle(float r)
+    {
+        radius = r;
+    }
+    public override void CalculateArea() 
+    {
+        Console.WriteLine($"Area of circle: {3.14 * radius * radius}");
+    }
+}
